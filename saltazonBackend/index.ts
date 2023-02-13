3a20d38415b5d5fe1f957dc1a89ff2976d6f1d9d
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-02-13 09:55:35 +0100

    adds timezone to backend time logging

--- saltazonBackend/index.ts
+++ saltazonBackend/index.ts
@@ -7,6 +7,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import fs from 'fs';
 import path from 'path';
+import moment from 'moment-timezone';
 import './middlewares/passport-local-login';
 import './middlewares/passport-local-register';
 
@@ -15,11 +16,12 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'backend-logging' , 'access.log'), { flags: 'a' })
- 
+
 app.use(express.json());
 app.use(cors());
 app.use(passport.initialize());
-app.use(morgan('Type :method, Date [:date[web]], StatusCode :status', { stream: accessLogStream }));
+morgan.token('date', () =>  moment().tz('Europe/Stockholm').format('YYYY-MM-DD HH:mm ZZ'))
+app.use(morgan('Type :method, Date [:date[Europe/Stockholm]], StatusCode :status', { stream: accessLogStream }));
 app.use('/user', userRouter);
 app.use('/product', productRouter);
 
