bd6362fd356399005a5b7547d55763f6cf9fe81f
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-02-20 11:25:25 +0100

    adds dynamic route - a product page

---
+++ saltazonFrontend/src/components/Navigation/CatewgoryDropdown/CategoryDropdown.tsx
@@ -0,0 +1,25 @@
+const CategoryDropdown = () => {
+  return (
+    <ul className="categories">
+      <li><label><input type="checkbox" /> Movies </label></li>
+      <li><label><input type="checkbox" /> Shoes </label></li>
+      <li><label><input type="checkbox" /> Grocery </label></li>
+      <li><label><input type="checkbox" /> Toys </label></li>
+      <li><label><input type="checkbox" /> Sports </label></li>
+      <li><label><input type="checkbox" /> Baby </label></li>
+      <li><label><input type="checkbox" /> Beauty </label></li>
+      <li><label><input type="checkbox" /> Games </label></li>
+      <li><label><input type="checkbox" /> Garden </label></li>
+      <li><label><input type="checkbox" /> Industrial </label></li>
+      <li><label><input type="checkbox" /> Clothing </label></li>
+      <li><label><input type="checkbox" /> Electronics </label></li>
+      <li><label><input type="checkbox" /> Kids </label></li>
+      <li><label><input type="checkbox" /> Computers </label></li>
+      <li><label><input type="checkbox" /> Tools </label></li>
+      <li><label><input type="checkbox" /> Books </label></li>
+      <li><label><input type="checkbox" /> Jewelry </label></li>
+    </ul>
+  )
+}
+
+export default CategoryDropdown
--- saltazonFrontend/src/components/Navigation/Navigation.tsx
+++ saltazonFrontend/src/components/Navigation/Navigation.tsx
@@ -1,23 +1,33 @@
+import './Navigation.css'
 import { Link } from 'react-router-dom';
+import CategoryDropdown from './CatewgoryDropdown/CategoryDropdown';
+import Cookies from 'js-cookie';
+
 
 const Navigation = () => {
+  const token = Cookies.get('token');
+
   return (
-    <nav className="navigation">
-      <ul className="navigation--sections">
-        <li>
-          <Link to={'/cart'} className={"nav_button"}>Cathegories</Link>
-        </li>
-        <li>
-          <Link to={'/'} className={"nav_button"}>SHOW ME ALL THE ITEMS</Link>
-        </li>
-        <li>
-          <Link to={'/login'} className={"nav_button"}>LogIn</Link>
-        </li>
-        <li>
-          <Link to={'/cart'} className={"nav_button"}>Go to cart</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navigation">
+        <ul className="navigation--sections">
+          <li className="dropdown">
+            <span className="dropdown--title">Categories</span>
+            { token && <CategoryDropdown />}
+          </li>
+
+          <li>
+            <Link to={'/'} className={"nav_button"}>SHOW ME ALL THE ITEMS</Link>
+          </li>
+          <li>
+            <Link to={'/login'} className={"nav_button"}>LogIn</Link>
+          </li>
+          <li>
+            <Link to={'/cart'} className={"nav_button"}>Go to cart</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
