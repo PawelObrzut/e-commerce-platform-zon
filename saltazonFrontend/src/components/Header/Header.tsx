6fe07ff67ea3d53188e94f4afadeae10c4ad2cf6
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-02-22 09:06:07 +0100

    adds log-out button

--- saltazonFrontend/src/components/Header/Header.tsx
+++ saltazonFrontend/src/components/Header/Header.tsx
@@ -1,8 +1,16 @@
+import Cookies from 'js-cookie'
+
 const Header = () => {
+  const handleLogOut = () => {
+    Cookies.remove('token')
+    Cookies.remove('email')
+    window.location.href = '/login';
+  }
+
   return (
     <header className="header">
       <h1>Saltazon</h1>
-      <p>Log out</p>
+      {Cookies.get('token') && <button className="logOut--btn" onClick={handleLogOut}>Log out</button>}
     </header>
   )
 }
