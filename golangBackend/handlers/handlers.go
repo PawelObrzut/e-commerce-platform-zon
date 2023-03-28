a14433190dbea1c17ed45b97e297a5d2ef38c6d7
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-03-28 13:40:29 +0200

    golang sends list of products in json to the client

--- golangBackend/handlers/handlers.go
+++ golangBackend/handlers/handlers.go
@@ -6,35 +6,42 @@ import (
 	"fmt"
 )
 
+type Product struct {
+	Id								int			`json:"id"`
+	Title							string	`json:"title"`
+	Description				string	`json:"description"`
+	Imageurl					string	`json:"imageUrl"`
+	Storeid						int			`json:"storeId"`
+	Price							string	`json:"price"`
+	Quantity					int			`json:"quantity"`
+	Category					string	`json:"category"`
+}
+
 func GetMainRoute(c *fiber.Ctx) error {
 	return c.SendString("Hello there general Kenobi")
 }
 
 func GetAllProducts(c *fiber.Ctx) error {
 	db, err := database.ConnectPostgres()
+
 	rows, err := db.Query("SELECT * FROM ProductData")
+	if err != nil {
+		fmt.Printf("Could not query ProductData: %v", err)
+		panic(err)
+	}
+
+	products := []Product{}
+
+	for rows.Next() {
+		var product Product
+		err := rows.Scan(&product.Id, &product.Title, &product.Description, &product.Imageurl, &product.Storeid, &product.Price, &product.Quantity, &product.Category); 
 		if err != nil {
-			fmt.Printf("Could not query ProductData: %v", err)
+			fmt.Printf("Could not scan ProductData rows: %v", err)
 			panic(err)
 		}
+		products = append(products, product)
+	}
+	defer db.Close()
 
-		for rows.Next() {
-			var id int
-			var title string
-			var description string
-			var imageurl string
-			var storeid int
-			var price string
-			var quantity int
-			var category string
-
-			err := rows.Scan(&id, &title, &description, &imageurl, &storeid, &price, &quantity, &category); 
-			if err != nil {
-				return nil
-			}
-
-			fmt.Println(id, title, description, imageurl, storeid, price, quantity, category + "\n");
-		}
-		defer db.Close()
-		return c.SendString("Hello, World 👋!!!!!!")
+	return c.JSON(products)
 }
