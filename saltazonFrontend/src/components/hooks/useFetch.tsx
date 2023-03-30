10cac57f14194faf3c6bc0974fa87f5c7528fb41
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-03-30 08:48:59 +0200

    improves useFetch

--- saltazonFrontend/src/components/hooks/useFetch.tsx
+++ saltazonFrontend/src/components/hooks/useFetch.tsx
@@ -1,19 +1,16 @@
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
-import { PaginateDetailsInterface } from '../../types';
 
 interface FetchData<T> {
   data?: T;
   isLoading: boolean;
   error: string | null;
-  details?: PaginateDetailsInterface
 }
 
 const useFetch = <T = unknown>(url: string): FetchData<T> => {
   const [data, setData] = useState<T | undefined>(undefined);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [details, setDetails] = useState({} as PaginateDetailsInterface);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,13 +23,7 @@ const useFetch = <T = unknown>(url: string): FetchData<T> => {
           }
         });
         const json = await response.json();
-        json.responseData && setData(json.responseData);
-        json && setDetails({
-          limit: json.limit,
-          page: json.page,
-          next: json.next,
-          count: json.count
-        })
+        json && setData(json)
         setIsLoading(false);
       } catch (error) {
         setIsLoading(false);
@@ -49,7 +40,7 @@ const useFetch = <T = unknown>(url: string): FetchData<T> => {
     };
   }, [url]);
 
-  return { data, isLoading, error, details };
+  return { data, isLoading, error };
 };
 
 export default useFetch;
