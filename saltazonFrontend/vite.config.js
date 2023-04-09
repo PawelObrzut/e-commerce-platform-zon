19aafea528dca7be434636a48f21e5d18f4a8882
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-04-09 22:49:15 +0200

    adds rollupOptions to vite.config.json

--- saltazonFrontend/vite.config.js
+++ saltazonFrontend/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+  build: {
+    outDir: 'dist',
+    minify: true,
+    rollupOptions: {
+      input: 'src/main.tsx',
+      output: {
+        format: 'esm',
       },
     },
   },
