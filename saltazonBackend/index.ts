a1eb6153a9bd0f1cbc39e8ba23f4bb2fe6331f5e
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-04-10 20:51:36 +0200

    sets cors

--- saltazonBackend/index.ts
+++ saltazonBackend/index.ts
@@ -5,7 +5,6 @@ import moment from 'moment-timezone';
 import path from 'path';
 import fs from 'fs';
 import passport from 'passport';
-import cors from 'cors';
 import morgan from 'morgan';
 import userRouter from './routes/user';
 import productRouter from './routes/product';
@@ -20,16 +19,23 @@ const app: Express = express();
 const port = process.env.PORT;
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'backend-logging', 'access.log'), { flags: 'a' });
 
+const allowedOrigin = 'https://tradezon-vite.onrender.com';
+
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin", "https://tradezon-vite.onrender.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", 'true');
+  const origin = req.headers.origin;
+  console.log('cors... req.headers.origin - ', origin);
+  if (origin === allowedOrigin) {
+    res.header("Access-Control-Allow-Origin", allowedOrigin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", 'true');
+  } else {
+    res.status(403).send('Forbidden');
+  }
   next();
 });
 
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:4173', 'https://tradezon-vite.onrender.com'], credentials: true }));
 app.use(cookieParser());
 app.use(passport.initialize());
 morgan.token('date', () => moment().tz('Europe/Stockholm').format('YYYY-MM-DD HH:mm ZZ'));
