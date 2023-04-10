6cbac250c0b88038a819fc5f3afb6a0f04c2e7a2
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-04-10 17:46:24 +0200

    changes filenames to lowercase

--- saltazonFrontend/src/main.tsx
+++ saltazonFrontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app';
 import { CartProvider } from './components/context/cartContext';
 import { AuthProvider } from './components/context/authContext';
 import './index.css';
