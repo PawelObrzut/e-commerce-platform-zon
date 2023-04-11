91b4fb66c54c3fd4a0516318ec1944e1f5369c44
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-04-11 10:43:12 +0200

    fixes wrong origin url

--- saltazonBackend/index.ts
+++ saltazonBackend/index.ts
@@ -19,17 +19,19 @@ const app: Express = express();
 const port = process.env.PORT;
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'backend-logging', 'access.log'), { flags: 'a' });
 
-// app.use(function(req, res, next) {
-//   const allowedOrigins = ['https://tradezon-node.onrender.com'];
-//   const origin = req.headers.origin;
-//   if (origin && allowedOrigins.includes(origin)) {
-//        res.setHeader('Access-Control-Allow-Origin', origin);
-//   }
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   res.header('Access-Control-Allow-credentials', 'true');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
-//   next();
-// });
+app.use(function(req, res, next) {
+  const allowedOrigins = ['https://tradezon-vite.onrender.com'];
+  const origin = req.headers.origin;
+  console.log('setting cors');
+  if (origin && allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
+  console.log('cors: res.header is set');
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
