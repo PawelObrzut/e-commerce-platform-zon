416ad396f183052f76f3984f2f117012f44866de
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-04-21 15:25:24 +0200

    adds drop if statements to createTables script, improves populating dbMock.js script

--- postresDatabase/dbMock.js
+++ postresDatabase/dbMock.js
@@ -8,11 +8,14 @@ const Products = require('./mockData/Products_Mock_data.json');
 const Users = require('./mockData/User_Mock_data.json');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: 'localhost',
-  database: process.env.DB_NAME,
-  port: 5432,
+	user: '<USER>',
+	password: '<PASSWORD>',
+	host: '<HOST>.frankfurt-postgres.render.com',
+	database: '<DATABASE>',
+	port: 5432,
+	ssl: {
+		rejectUnauthorized: false
+	}
 });
 
 (async () => {
@@ -22,20 +25,26 @@ const pool = new Pool({
       const store = Stores[i];
       client.query('INSERT INTO StoreData (name, uniqueStoreId) VALUES ($1, $2)', [store.name, store.uniqueStoreId]);
     }
-
-    for (let i = 0; i < Products.length; i ++) {
-      const product = Products[i];
-      client.query('INSERT INTO ProductData (title, description, imageUrl, storeId, price, quantity, category) VALUES ($1, $2, $3, $4, $5, $6, $7)', [product.title, product.description, product.imageUrl, product.storeId, product.price, product.quantity, product.category]);
-    }
+    console.log('store data ready')
 
     for (let i = 0; i < Users.length; i ++) {
       const user = Users[i];
       const hashedPassword = bcrypt.hashSync(user.password, 10);
       client.query('INSERT INTO UserData (email, password, role, storeid) VALUES ($1, $2, $3, $4)', [user.email, hashedPassword, user.role, user.uniqueStoreId]);
+      console.log(i);
     }
+    console.log('user data ready');
+
+    for (let i = 0; i < Products.length; i ++) {
+      const product = Products[i];
+      client.query('INSERT INTO ProductData (title, description, imageUrl, storeId, price, quantity, category) VALUES ($1, $2, $3, $4, $5, $6, $7)', [product.title, product.description, product.imageUrl, product.storeId, product.price, product.quantity, product.category]);
+      console.log(i);
+    }
+    console.log('product data ready');
 
   } catch (err) {
     console.log(err.stack)
+    console.log('error!')
   } finally {
     console.log('finished')
     // client.release();
