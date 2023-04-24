c1b21a92135f7f7b6b461f150692294e0742f986
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-04-24 09:45:55 +0200

    adds delete option in the storePanel

--- saltazonFrontend/src/components/pages/adminPage.tsx
+++ saltazonFrontend/src/components/pages/adminPage.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import useAuth from '../hooks/useAuth';
 import useFetch from '../hooks/useFetch';
 import Spinner from '../Spinner/spinner';
 import { baseURL } from '../utils/api';
 import { ProductInterface } from '../../types';
 import { fakeProducts } from '../../fakedata/fakedata';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import axios from 'axios';
+import DeleteModal from '../DeleteModal/deleteModal';
 
 interface StoreInterface {
   uniqueStoreId: number,
@@ -15,23 +18,10 @@ interface StoreInterface {
 const AdminPage = () => {
   const { user } = useAuth();
   const [updates, setUpdates] = useState<Partial<ProductInterface>>({});
-
-  const handleUpdate = (productId: number, field: keyof ProductInterface, value: string | number) => {
-    setUpdates({
-      ...updates,
-      [productId]: {
-        ...updates[productId],
-        [field]: value,
-      },
-    });
-  };
-
-  const handleSave = () => {
-    console.log('not implemented yet')
-  }
-
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [selectProduct, setSelectProduct] = useState<number>(null);
   const url = `${baseURL}/store/${user.storeId}`;
-  const { data, isLoading, error } = useFetch<StoreInterface>(url);
+  const { data, isLoading, error, setData } = useFetch<StoreInterface>(url);
 
   if (isLoading) {
     return (
@@ -45,10 +35,45 @@ const AdminPage = () => {
     return <p>{error}</p>;
   }
 
-  // console.log(fakeProducts);
-  // useEffect(() => {
-  //   console.log(updates);
-  // }, [updates])
+  const handleUpdate = (productId: number, field: keyof ProductInterface, value: string | number) => {
+    setUpdates({
+      ...updates,
+      [productId]: {
+        ...updates[productId],
+        [field]: value,
+      },
+    });
+  }
+
+  const handleSave = (productId: number) => {
+    console.log('not implemented yet')
+    console.log(updates[productId])
+  }
+
+  const handleShowModal = (productId: number) => {
+    setConfirmDelete(true)
+    setSelectProduct(productId);
+  }
+
+  const handleDelete = (productId: number) => {
+    axios({
+      method: 'delete',
+      url: `${baseURL}/product/${productId}`
+    })
+      .then(response => {
+        if (response.status === 204) {
+          setData({
+            ...data,
+            products: data.products.filter(
+              (product: ProductInterface) => product.id !== productId
+            )
+          });
+        } else {
+          console.log(`Error deleting product with ID ${productId}.`);
+        }
+      })
+      .catch(error => console.log(error));
+  }
 
   return (
     <main className='min-h-screen p-5'>
@@ -67,6 +92,7 @@ const AdminPage = () => {
         <table className='w-full'>
           <thead className='border border-black '>
             <tr>
+              <th className='p-2 font-normal'></th>
               <th className='p-2 font-normal'>Image</th>
               <th className='p-2 font-normal'>Id</th>
               <th className='p-2 font-normal'>Title</th>
@@ -79,10 +105,16 @@ const AdminPage = () => {
           </thead>
           <tbody>
             {
+              // data.products
+              // fakeProducts
               data.products.map((product: ProductInterface) => (
                 <tr key={product.id} className='hover:bg-gray-100 '>
+                  <td className='p-2'>
+                    <RiCloseCircleLine onClick={() => handleShowModal(product.id)} size={30} className='text-blue-600 hover:text-red-600 cursor-pointer' />
+                    { confirmDelete && <DeleteModal handleDelete={handleDelete} setConfirmDelete={setConfirmDelete} product={selectProduct} /> }
+                  </td>
                   <td><img src={product.imageUrl} alt="product" className='h-32 object-scale-down mx-auto' /></td>
-                  <td>{product.id}</td>
+                  <td className='p-2'>{product.id}</td>
                   <td>{product.title}</td>
                   <td>{product.description}</td>
                   <td>
@@ -107,7 +139,9 @@ const AdminPage = () => {
                   </td>
                   <td>{product.category}</td>
                   <td>
-                    <button onClick={handleSave} className='block p-2 border rounded-md'>Save</button>
+                    {
+                      updates[product.id] &&  <button onClick={() => handleSave(product.id)} className='block p-2 border rounded-md bg-orange-300 hover:bg-orange-400'>Save</button>
+                    }
                   </td>
                 </tr>
               ))
