21bb8c936400ecc6ce45ce10bac5b9cf749312bf
Pawel Obrzut <pawel.r.obrzut@gmail.com>
2025-12-09 19:20:13 +0100

    Update api.ts

--- saltazonFrontend/src/components/utils/api.ts
+++ saltazonFrontend/src/components/utils/api.ts
@@ -1,3 +1,3 @@
-export const baseURL = 'https://e-commerce-platform-zon-production-9405.up.railway.app';
+export const baseURL = 'node-production-bc38.up.railway.app';
 // export const baseURL = 'http://localhost:8080';
 // export const baseURL = 'https://tradezon-node.onrender.com';
