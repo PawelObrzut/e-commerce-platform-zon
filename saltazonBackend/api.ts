a1ee99d5bf3b6582fe60ecebe763ac84b6fd36ab
Pawel Obrzut <pawel.r.obrzut@gmail.com>
2025-12-10 08:20:22 +0100

    Update api.ts

--- saltazonBackend/api.ts
+++ saltazonBackend/api.ts
@@ -1,6 +1,6 @@
 //const baseURL = 'http://127.0.0.1:8000';
 //const baseURL = 'https://tradezon-go.onrender.com';
 
-const baseURL = 'https://golang-production-7d21.up.railway.app/';
+const baseURL = 'https://golang-production-7d21.up.railway.app';
 
 export default baseURL;
